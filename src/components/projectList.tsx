68f3896ae56d06719107fad546bc2cfb52d45378
ericvini000 <ericviniciusfelixsilva@gmail.com>
2023-12-15 10:00:56 -0400

    fix: repos error fixed

--- src/components/projectList.tsx
+++ src/components/projectList.tsx
@@ -4,15 +4,15 @@ import ProjectListItem from "./projectListItem"
 
 const ProjectList = async () => {
 
-    // const repos: Repository[] = await fetchRepositories()
+    const repos: Repository[] = await fetchRepositories()
 
     return (
         <ul className="flex flex-col lg:flex-row lg:gap-y-16 lg:flex-wrap gap-8 overflow-auto max-h-[600px] scrollbar scrollbar-thin scrollbar-track-gray-400 scrollbar-track-rounded-md scrollbar-thumb-primary-100">
-            {/* {
+            {
                 repos.map((element, index) => (
-                    <ProjectListItem key={index + 0} name={element.name} description={element.description} html_url={element.html_url} app_url={"app"} language={element.language} />
+                    <ProjectListItem key={index + 1} name={element.name} description={element.description} html_url={element.html_url} app_url={"app"} language={element.language} />
                 ))
-            } */}
+            }
         </ul>
     )
 }
