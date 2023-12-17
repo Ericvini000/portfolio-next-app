881c8091c33c27283dcf03970d942b75b85a3f37
ericvini000 <ericviniciusfelixsilva@gmail.com>
2023-12-16 21:55:52 -0400

    fix: allowing fetch request

--- src/components/projectList.tsx
+++ src/components/projectList.tsx
@@ -5,15 +5,15 @@ import ProjectListItem from "./projectListItem";
 
 const ProjectList = async () => {
 
-    // const repos: TRepositoryArray = await fetchRepositories();
+    const repos: TRepositoryArray = await fetchRepositories();
 
     return (
         <ul className="flex flex-col lg:flex-row lg:gap-y-16 lg:flex-wrap gap-8 overflow-auto max-h-[600px] scrollbar-thin scrollbar-track-gray-400 scrollbar-track-rounded-md scrollbar-thumb-primary-100">
-            {/* {
+            {
                 repos.map((element, index) => (
                     <ProjectListItem key={index + 1} project={element} />
                 ))
-            } */}
+            }
         </ul>
     )
 }
